2bdff4509668af6b119e3353e73b7a9560dd3689
Chanju Park <chanjure@snu.ac.kr>
2022-01-03 15:05:03 +0900

    MC analysis tools

--- LGT/mc.py
+++ LGT/mc.py
@@ -5,6 +5,9 @@
 # -------------------- #
 
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from . import Lattice
 from . import action
 
 #import err
@@ -39,9 +42,9 @@ def metropolis(lat, bare_args={'beta':1}):
 
 	Parameters
 	----------
-	latt Lattice
+	lat Lattice
 		lattice to update
-	action_type str
+	g Action
 		action type
 	discard int
 		number of discard step
@@ -57,7 +60,7 @@ def metropolis(lat, bare_args={'beta':1}):
 	"""
 
 	# MC parameter settings
-	G = action(lat, bare_args)
+	g = action(lat,bare_args)
 	beta = bare_args["beta"]
 	
 	#TODO add sweep scheme as an option
@@ -65,14 +68,136 @@ def metropolis(lat, bare_args={'beta':1}):
 	
 	for mu in range(lat.aux_index):
 		for i in range(lat.lat_size):
-			dS, new_field = G.DS(lat.field,mu)
+			dS, new_field = g.DS(lat.field,mu)
 
 			if np.random.rand() < np.exp(-beta*dS):
 				accept = 1
 				lat.field = new_field
 
 	return (lat.field, accept)
-		
-	
 
-		
+def calc_teq(bare_arg, O, init_lat, mcstep=metropolis, tol=1e-3, max_step=200, verbose=0, fig_title=None):
+
+	t_eq = max_step
+
+	O_cold = []
+	O_hot = []
+	
+	lat_shape = init_lat.lat_shape
+	field_type = init_lat.field_type
+	seed = init_lat.seed
+
+	cold_init = Lattice(lat_shape)
+	cold_init.init_fields(field_type,'Cold',seed)
+
+	hot_init = Lattice(init_lat.lat_shape)
+	hot_init.init_fields(field_type,'Hot',seed)
+
+	diff_temp = 10*tol
+	cold_res_temp = 0.
+
+	for i in range(max_step):
+		mcstep(cold_init, bare_arg)
+		mcstep(hot_init, bare_arg)
+
+		cold_res = O(cold_init.field)
+		hot_res = O(hot_init.field)
+
+		diff = np.abs(cold_res - hot_res)
+		O_cold.append(cold_res)
+		O_hot.append(hot_res)
+
+		if diff < tol and np.abs(cold_res - cold_res_temp) < tol:
+			t_eq = i
+			break
+
+		diff_temp = diff
+		cold_res_temp = cold_res
+
+	if verbose :
+		plt.title("Estimation of thermalizationtion time")
+		plt.plot(np.arange(len(O_cold)),O_cold,'C0',label='cold')
+		plt.plot(np.arange(len(O_hot)),O_hot,'C3',label='hot')
+		plt.legend(loc='upper right')
+		plt.xlabel("Monte Carlo time")
+		plt.ylabel("Observable")
+		plt.grid(True)
+		plt.show()
+		if fig_title is not None:
+			plt.savefig(fig_title)
+
+	return t_eq
+
+def autocorrelation(conf, O, t):
+	N = len(conf) - t
+	eps = 1e-7
+
+	o1o2 = 0.
+	o1 = 0.
+	o2 = 0.
+
+	o1o1 = 0.
+	o2o2 = 0.
+
+	for i in range(N):
+
+		o1o2 += O(conf[i])*O(conf[i+t])/N
+
+		o1 += O(conf[i])/N
+		o2 += O(conf[i+t])/N
+
+		o1o1 += O(conf[i])*O(conf[i])/N
+
+	cor_t = o1o2 - o1*o2
+
+	return cor_t/(o1o1 - o1*o1 + eps)
+
+def calc_tac(bare_arg, O, init_lat, mcstep=metropolis, t_eq=100, n_conf_ac=500, verbose=0, fig_title=None):
+
+	def fit_func(x,a,b):
+		return a*np.exp(-x/b)
+
+	lat_shape = init_lat.lat_shape
+	field_type = init_lat.field_type
+	seed = init_lat.seed
+
+	conf_ac = []
+
+	ac_init = Lattice(lat_shape)
+	ac_init.init_fields(field_type,'Cold',seed)
+
+	for i in range(t_eq):
+		mcstep(ac_init, bare_arg)
+
+	for e in range(n_conf_ac):
+		mcstep(ac_init, bare_arg)
+		conf_ac.append(ac_init.field)
+
+	ac_hist = np.empty(n_conf_ac)
+
+	for i in range(n_conf_ac):
+		ac_hist[i] = autocorrelation(conf_ac, O, i)
+	
+	fit_range = n_conf_ac - int(n_conf_ac/5)
+	fit_b, fit_cov = curve_fit(fit_func,np.arange(fit_range),ac_hist[:fit_range])
+	
+	tac = fit_b[1]
+
+	beta = bare_arg['beta']
+	if verbose :
+		x = np.arange(n_conf_ac)
+		y = fit_func(x,fit_b[0],fit_b[1])
+
+		plt.title(r"Autocorrelation plot $\beta$=%0.3f $t_{ac}$=%0.3f"%(beta,tac))
+		plt.plot(x, ac_hist, 'C0o')
+		plt.plot(x,y,'C3')
+		plt.xlabel("Monte Carlo time")
+		plt.ylabel("Autocorrelation")
+		plt.grid("True")
+		plt.show()
+		if fig_title is not None:
+			plt.savefig(fig_title)
+
+	return tac
+
+
